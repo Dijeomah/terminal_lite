0184b6d4d0d378d7877d280fc2f32cee1a55245e
Drame Success <d.ije95@gmail.com>
2023-06-08 15:10:42 +0100

    Test

--- db/conn.php
+++ db/conn.php
@@ -1,11 +1,11 @@
 <?php
 
 //grab all rows from the table where state is "new" and put into array
-$host = 'db-mysql-airvendb2b-do-user-4199791-0.b.db.ondigitalocean.com';
-$port = '25060';
-$username = 'doadmin';
-$password = 'zmJTs008ybcc7nND';
-$database = 'airvendb2b';
+$host = '';
+$port = '';
+$username = '';
+$password = '';
+$database = '';
 
 $conn = mysqli_connect($host, $username, $password, $database, $port);
 
